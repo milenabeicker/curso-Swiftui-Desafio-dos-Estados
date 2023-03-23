f7a37e16edc293bb497f593c2ed1176508b55636
Milena Kessia <milenakessia87@gmail.com>
2023-03-23 19:35:02 -0300

    Initial Commit

---
+++ Desafio dos Estados/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Desafio dos Estados
+//
+//  Created by Milena Kessia on 23/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Desafio dos Estados/Desafio_dos_EstadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio_dos_EstadosApp.swift
+//  Desafio dos Estados
+//
+//  Created by Milena Kessia on 23/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Desafio_dos_EstadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
