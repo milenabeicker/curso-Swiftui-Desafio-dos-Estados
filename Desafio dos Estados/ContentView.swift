885c975e875246685087c265aff7c6d8ac192dcb
Milena Kessia <milenakessia87@gmail.com>
2023-05-07 20:07:36 -0300

    Criação de repositório.

--- Desafio dos Estados/ContentView.swift
+++ Desafio dos Estados/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    var estados = ["DF": "Distrito Federal", "MG": "Minas Gerais", "RJ": "Rio de Janeiro", "SP": "São Paulo", "ES": "Espirito Santo", "SC": "Santa Catarina"]
+    
+    var tituloSection = ("ESTADOS", "BRASIL")
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+        Group{
+            List{
+                Section("\(tituloSection.0) do \(tituloSection.1) "){
+                    ForEach(Array(estados), id: \.0) { key, value in
+                        Text("\(value)")
+                            .onDisappear{
+                                print("\(value) desapareceu da tela")
+                            }
+                            .onAppear{
+                                print("\(key) apareceu na tela")
+                            }
+                    }
+                }
+            }
+            
+        }
+        .onAppear{
+            print("---------ESTADOS---------")
+            for (key, value) in estados {
+                print("\(key) - \(value)")
+            }
+            print("-------------------")
+        }
+   }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        }
     }
 }
